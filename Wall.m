2f6781e3d4940424554280077021748650fb2f45
Targamas <jonathan.cr1302@gmail.com>
2017-04-05 00:30:51 +0200

    Changement wall et ajour de liste de murs

--- Wall.m
+++ Wall.m
@@ -1,28 +1,30 @@
 classdef Wall
-    %Cette classe permets de contrstruire des murs de l'environements
-    
+    %Cette classe contient les propriétés des murs de l'environement
+
     properties
         x1 %Abscisse extrémité 1
         y1 %Ordonnée extrémité 1
         x2 %Abscisse extrémité 2
         y2 %Ordonnée extremité 2    
         perm %Permitivité
+        cond %Conductivité
         ep %Epaisseur
     end
     
     methods
         %Constructeur:
-        function obj = Wall(x1,y1,x2,y2,perm,ep)
+        function obj = Wall(x1,y1,x2,y2,perm,cond,ep)
             obj.x1 = x1;
             obj.y1 = y1;
             obj.x2 = x2;
             obj.y2 = y2;
             obj.perm = perm;
+            obj.cond = cond;
             obj.ep = ep;
         end
         
         function plot(obj)
-            plot([obj.x1,obj.y1], [obj.x2,obj.y2]);
+            plot([obj.x1,obj.x2], [obj.y1,obj.y2], 'Color', 'b');
             hold on;
         end
     end 
--- main.m
+++ main.m
@@ -1,13 +1,26 @@
-%Ceci est le script principal dans lequel est effectuÃ© la simulation
+%Ceci est le script principal dans lequel est effectuée la simulation
 
-wall1 = Wall(0,0,0,20,12,6); 
-wall2 = Wall(20,0,20,20,12,6); 
-wall3 = Wall(0,20,20,20,12,6); 
-wall4 = Wall(0,0,20,0,12,6); 
-wall5 = Wall(1,23,4,7,12,6);
+%Initialisation des constantes:
 
-wall1.plot();
-wall2.plot();
-wall3.plot();
-wall4.plot();
-wall5.plot();
+epsMur = 5; %Permitivité relative des murs (en béton)
+sigmaMur = 0.014; %Conductivité des murs (en béton)
+L = 50; %Longueur caractéristique du plan(m)
+e = 0.4; %Epaisseur mur (m)
+
+%Construction des objets murs de l'environement
+
+wall1 = Wall(0,0,0,L,epsMur, sigmaMur,e); 
+wall2 = Wall(0,0,L,0,epsMur, sigmaMur,e); 
+wall3 = Wall(L,0,L,L,epsMur, sigmaMur,e); 
+wall4 = Wall(0,L,L,L,epsMur, sigmaMur,e); 
+
+%Création d'un liste contenant les murs: 
+wallList = [wall1, wall2, wall3, wall4];
+
+%Affichage des murs:
+
+for i = 1:numel(wallList)
+    wallList(i).plot();
+end
+
+
