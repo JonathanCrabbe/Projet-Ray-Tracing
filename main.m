b0e85bddb4a8e1a4609c32117c4e63ed9570bc83
celiagriffet <celiagriffet@gmail.com>
2017-04-05 17:57:17 +0200

    Test

--- main.m
+++ main.m
@@ -1,14 +1,15 @@
-%Ceci est le script principal dans lequel est effectuée la simulation
+%Ceci est le script principal dans lequel est effectuï¿½e la simulation
+
 
 %Initialisation des constantes:
 
-epsMur = 5; %Permitivité relative des murs (en béton)
-sigmaMur = 0.014; %Conductivité des murs (en béton)
-L = 50; %Longueur caractéristique du plan(m)
+epsMur = 5; %Permitivitï¿½ relative des murs (en bï¿½ton)
+sigmaMur = 0.014; %Conductivitï¿½ des murs (en bï¿½ton)
+L = 50; %Longueur caractï¿½ristique du plan(m)
 e = 0.4; %Epaisseur mur (m)
-c = 299792458; %Vitesse de la lumière dans le vide (m/s)
-f = 2.45*10^9; %Fréquence des communications (Hertz) 
-lambda = c/f; %Longueur d'onde rayonnée (m)
+c = 299792458; %Vitesse de la lumiï¿½re dans le vide (m/s)
+f = 2.45*10^9; %Frï¿½quence des communications (Hertz) 
+lambda = c/f; %Longueur d'onde rayonnï¿½e (m)
 %Construction des objets murs de l'environement
 
 wall1 = Wall(0,0,0,L,epsMur, sigmaMur,e); 
@@ -16,7 +17,7 @@ wall2 = Wall(0,0,L,0,epsMur, sigmaMur,e);
 wall3 = Wall(L,0,L,L,epsMur, sigmaMur,e); 
 wall4 = Wall(0,L,L,L,epsMur, sigmaMur,e); 
 
-%Création d'un liste contenant les murs: 
+%Crï¿½ation d'un liste contenant les murs: 
 wallList = [wall1, wall2, wall3, wall4];
 
 %Affichage des murs:
